d691dae27eb2e7051e4c7d63de6ceeb51d7da2b1
oasj <oasjatoba1@gmail.com>
2022-09-28 10:14:24 -0300

    All files complete, but some functions still failing. Current score: 8,5.

---
+++ POO II/VPL_02/biblioteca.py
@@ -0,0 +1,29 @@
+from livro import Livro
+
+
+class Biblioteca:
+    def __init__(self):
+        self.__livros = []
+
+    def incluirLivro(self, livro: Livro):
+        #Nao esqueca de garantir que o objeto recebido pertence a classe Livro...
+        if isinstance(livro, Livro):
+            if livro not in self.__livros:
+                self.__livros.append(livro)
+            else:
+                print("Livro já cadastrado")
+        else:
+            raise TypeError("O objeto não é do tipo Livro")
+
+    def excluirLivro(self, livro: Livro):
+        if isinstance(livro, Livro):
+            if livro in self.__livros:
+                self.__livros.remove(livro)
+            else:
+                print("Livro não cadastrado")
+        else:
+            raise TypeError("O objeto não é do tipo Livro")
+
+    @property
+    def livros(self):
+        return self.__livros
---
+++ POO II/VPL_02/livro.py
@@ -0,0 +1,88 @@
+from editora import Editora
+from autor import Autor
+from capitulo import Capitulo
+
+class Livro:
+    def __init__(self, codigo: int, titulo: str, ano: int, editora: Editora, autor: Autor, numeroCapitulo: int, tituloCapitulo: str):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__editora = editora    
+        self.__autores = []
+        self.__capitulos = []
+        self.incluirAutor(autor)
+        self.incluirCapitulo(numeroCapitulo, tituloCapitulo)
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @property
+    def editora(self):
+        return self.__editora
+
+    @property
+    def autores(self):
+        return self.__autores
+
+
+    @codigo.setter
+    def codigo(self, codigo):
+        self.__codigo = codigo
+
+    @titulo.setter
+    def titulo(self, titulo):
+        self.__titulo = titulo
+
+    @ano.setter
+    def ano(self, ano):
+        self.__ano = ano
+
+    @editora.setter
+    def editora(self, editora: Editora):
+        self.__editora = editora
+
+
+    def incluirAutor(self, autor: Autor):
+        # Nao esqueca de garantir que o objeto recebido pertence a classe Autor...
+        # Nao permitir insercao de Autores duplicados!
+        if isinstance(autor, Autor):    
+            if autor not in self.__autores:
+                self.__autores.append(autor)
+            else:
+                print("Autor já cadastrado!")
+        else:
+            print("O objeto não pertence a classe Autor!")
+
+    def excluirAutor(self, autor: Autor):
+        if autor in self.__autores:
+            self.__autores.remove(autor)
+        else:
+            print("Autor não cadastrado!")
+
+    def incluirCapitulo(self, numeroCapitulo: int, tituloCapitulo: str):
+        # Nao permitir insercao de Capitulos duplicados!
+        if self.findCapituloByTitulo(tituloCapitulo) == None:
+            self.__capitulos.append(Capitulo(numeroCapitulo, tituloCapitulo))
+        else:
+            print("Capítulo já existe!")
+
+    def excluirCapitulo(self, tituloCapitulo: str):
+        if self.findCapituloByTitulo(tituloCapitulo) != None:
+            self.__capitulos.remove(self.findCapituloByTitulo(tituloCapitulo))
+        else:
+            print("Capítulo não existe!")
+
+    def findCapituloByTitulo(self, tituloCapitulo: str):
+        if tituloCapitulo in self.__capitulos:
+            return tituloCapitulo
+        else:
+            return None
